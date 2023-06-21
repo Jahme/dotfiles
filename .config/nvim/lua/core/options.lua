b9249b07b49f92cdc63c0c91537acb20fb5c7fa2
Jahme <jchomchan32@gmail.com>
2023-06-21 08:24:30 -0700

    chore(config): Lazy Sync. Updated options and themes

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -2,9 +2,14 @@ local opts = {
   shiftwidth = 2,
   tabstop = 2,
   expandtab = true,
+  autoindent = true,
+  smartindent = true,
+  smarttab = true,
+  scrolloff = 10,
   wrap = false,
   number = true,
   relativenumber = true,
+  cursorline = true,
 }
 
 for opt, val in pairs(opts) do
